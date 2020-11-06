905afaec359141dd4dc969482cee5a544a2b2328
n-ii-lo <alyaska56@gmail.com>
2020-11-06 21:16:58 +0200

    testing release

---
+++ flutter test app 1/flutter test app 1/flutter_test_app_1App.swift
@@ -0,0 +1,17 @@
+//
+//  flutter_test_app_1App.swift
+//  flutter test app 1
+//
+//  Created by Danylo Chuprov on 05.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct flutter_test_app_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ flutter test app 1/flutter test app 1Tests/flutter_test_app_1Tests.swift
@@ -0,0 +1,33 @@
+//
+//  flutter_test_app_1Tests.swift
+//  flutter test app 1Tests
+//
+//  Created by Danylo Chuprov on 05.11.2020.
+//
+
+import XCTest
+@testable import flutter_test_app_1
+
+class flutter_test_app_1Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
